6a371105160a48420ceaaf0b99cac07e6921275c
Valeriano A.R <valen.alfonso@gmail.com>
2017-09-14 17:27:16 +0200

    Add FrmBase64

---
+++ VAR.Toolbox/FrmBase64.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VAR.Toolbox
+{
+    public partial class FrmBase64 : Form
+    {
+        public FrmBase64()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Codifica una cadena en base64.
+        /// </summary>
+        /// <param name="toEncode">Cadena a codificar</param>
+        /// <returns>Cadena codificada</returns>
+        static public string Base64Encode(string toEncode)
+        {
+            byte[] toEncodeAsBytes
+                  = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue
+                  = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Decodifica una cadena desde base64.
+        /// </summary>
+        /// <param name="encodedData">Cadena codificada</param>
+        /// <returns>Cadena decodificada</returns>
+        static public string Base64Decode(string encodedData)
+        {
+            byte[] encodedDataAsBytes
+                = System.Convert.FromBase64String(encodedData);
+            string returnValue =
+               System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+            return returnValue;
+        }
+
+        private void btnDecodeBase64_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = Base64Decode(txtInput.Text);
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void btnEncodeBase64_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = Base64Encode(txtInput.Text);
+        }
+    }
+}
--- VAR.Toolbox/FrmToolbox.cs
+++ VAR.Toolbox/FrmToolbox.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace VAR.Toolbox
 {
-    public partial class Form1 : Form
+    public partial class FrmToolbox : Form
     {
-        public Form1()
+        public FrmToolbox()
         {
             InitializeComponent();
+        }
+
+        private void btnBase64_Click(object sender, EventArgs e)
+        {
+            var frmBase64 = new FrmBase64();
+            frmBase64.Show();
         }
     }
 }
