1a46040210390410c47b699ff48e78413a7207ca
Valeriano A.R <valen.alfonso@gmail.com>
2019-11-01 02:38:28 +0100

    DummyBot: Even more dummy.

--- VAR.ScreenAutomation/Bots/DummyBot.cs
+++ VAR.ScreenAutomation/Bots/DummyBot.cs
@@ -5,26 +5,23 @@ namespace VAR.ScreenAutomation.Bots
 {
     public class DummyBot : IAutomationBot
     {
-        private int frameCount = 0;
-
         public string Name => "Dummy";
 
         public void Init(IOutputHandler output)
         {
-            frameCount = 0;
             output.Clean();
         }
 
         public Bitmap Process(Bitmap bmpInput, IOutputHandler output)
         {
-            frameCount++;
-            output.AddLine(string.Format("Frame: {0}", frameCount));
             return bmpInput;
         }
 
         public string ResponseKeys()
         {
-            return "{UP}";
+            // https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys?view=netframework-4.8
+            //return "{UP}";
+            return string.Empty;
         }
     }
 }
