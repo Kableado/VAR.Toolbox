8e957dc0546a3b127f6970f5c459868c2bd868a7
Valeriano A.R <valen.alfonso@gmail.com>
2020-07-20 20:50:27 +0200

    ControlsUtils.SetControlPropertyThreadSafeDelegate: Method to change properties of controls in a thread safe way.

--- VAR.Toolbox/Controls/ControlsUtils.cs
+++ VAR.Toolbox/Controls/ControlsUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Reflection;
+using System.Windows.Forms;
 
 namespace VAR.Toolbox.Controls
 {
@@ -7,6 +8,33 @@ namespace VAR.Toolbox.Controls
         public static float GetFontSize(Control ctrl, float size)
         {
             return size * 96f / ctrl.CreateGraphics().DpiX;
+        }
+
+        private delegate void SetControlPropertyThreadSafeDelegate(
+            Control control,
+            string propertyName,
+            object propertyValue);
+
+        public static void SetControlPropertyThreadSafe(
+            Control control,
+            string propertyName,
+            object propertyValue)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new SetControlPropertyThreadSafeDelegate
+                (SetControlPropertyThreadSafe),
+                new object[] { control, propertyName, propertyValue });
+            }
+            else
+            {
+                control.GetType().InvokeMember(
+                    propertyName,
+                    BindingFlags.SetProperty,
+                    null,
+                    control,
+                    new object[] { propertyValue });
+            }
         }
     }
 }
